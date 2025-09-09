74f5408f596c54326606d5078e17c19045d75db8
Niko <n95031648@gmail.com>
2025-09-09 23:19:55 +0200

    feat: added Discord RPC integration

--- vite.main.config.ts
+++ vite.main.config.ts
@@ -1,14 +1,25 @@
 import { defineConfig } from "vite";
-
+import path from "path"; 
 export default defineConfig({
-  build: {
-    lib: {
-      entry: "src/main.ts",
-      formats: ["cjs"],
+  optimizeDeps: {
+    exclude: ["discord-rpc", "ws", "bufferutil", "utf-8-validate"],
+  },
+  resolve: {
+    alias: {
+      "register-scheme": path.resolve(__dirname, "src/empty-register-scheme.ts"),
+      bufferutil: false as any,
+      "utf-8-validate": false as any,
     },
+  },
+  build: {
+    lib: { entry: "src/main.ts", formats: ["cjs"] }, // keep CJS
     outDir: ".vite/build",
     rollupOptions: {
-      external: ["electron"],
+      external: ["electron", "discord-rpc", "ws", "bufferutil", "utf-8-validate"],
+    },
+    commonjsOptions: {
+      ignoreTryCatch: false,
+      transformMixedEsModules: true,
     },
   },
 });
